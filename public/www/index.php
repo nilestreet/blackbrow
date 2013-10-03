9d30829d611f7adad128fde4165a233e67175ab1
Drew <drew@nilestreet.com.au>
2013-10-03 16:56:49 +0930

    Initial commit

---
+++ public/www/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Black brow</title>
+<style type="text/css">
+<!--
+#divBird {
+	margin-right: auto;
+	margin-left: auto;
+	width: 700px;
+}
+-->
+</style>
+</head>
+
+<body>
+<div id="divBird"><img src="images/bird.jpg" alt="" /></div>
+</body>
+</html>
